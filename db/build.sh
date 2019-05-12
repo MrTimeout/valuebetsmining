2a175fe7b31105e88df3884a5dd96993fc52d1c1
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-05-12 18:28:19 +0200

    Creating and updating db/build.sh

--- db/build.sh
+++ db/build.sh
@@ -1,10 +1,9 @@
-
 #!/usr/bin/env bash
 
-CONTAINER=golang_project_container
-IMAGE=golang_project_image
+CONTAINER=postgreSQL_project_container
+IMAGE=postgreSQL_project_image
 TAG=first
-FILE=src/run.sh
+NETWORK=
 
 if [ $(docker container ls --all --format "{{.Names}}" | grep -w -c $CONTAINER) -ge 1 ]; then
     if [ $(docker container ls --format "{{.Names}}" | grep -w -c $CONTAINER) -ge 1 ]; then 
@@ -13,13 +12,8 @@ if [ $(docker container ls --all --format "{{.Names}}" | grep -w -c $CONTAINER) 
     docker container rm $CONTAINER     
 fi
 
-if [ -f "$FILE" ]; then
-    chmod +x $FILE
-fi
-
-
 docker build --tag $IMAGE:$TAG --no-cache --rm .
 
 if [ $(docker image ls --all --filter "reference=$IMAGE:$TAG" --format "{{.Repository}}:{{.Tag}}" | grep -w -c $IMAGE:$TAG) -eq 1 ]; then
-    docker run -d --name $CONTAINER --network net -p 3000-4000:8080 $IMAGE:$TAG
+    docker run -d --name $CONTAINER --network  -p 3000-4000:8080 $IMAGE:$TAG
 fi
