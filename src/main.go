c29eb540ecfc513fb25a09616e66ba86695634f0
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-05-15 17:10:45 +0200

    Finihing it

--- src/main.go
+++ src/main.go
@@ -9,6 +9,7 @@ import (
 	"valuebetsmining/src/gql"
 	"valuebetsmining/src/postgres"
 	"valuebetsmining/src/server"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
@@ -48,13 +49,13 @@ func initializeAPI() (*chi.Mux, *postgres.Db) {
 
 	router.Use(
 		render.SetContentType(render.ContentTypeJSON), // set content-type headers as application/json
-		middleware.Logger,          // log api request calls
-		middleware.DefaultCompress, // compress results, mostly gzipping assets and json
-		middleware.StripSlashes,    // match paths with a trailing slash, strip it, and continue routing through the mux
-		middleware.Recoverer,       // recover from panics without crashing server
+		middleware.Logger,                             // log api request calls
+		middleware.DefaultCompress,                    // compress results, mostly gzipping assets and json
+		middleware.StripSlashes,                       // match paths with a trailing slash, strip it, and continue routing through the mux
+		middleware.Recoverer,                          // recover from panics without crashing server
 	)
 
-	router.Post("/users", s.GraphQL())
+	router.Post("/users", s.GraphQL()) //curl -d '{  "query": "{users (name:\"kevin\"){id, name, age}}" }' http://localhost:3000/users
 
 	return router, db
 }
