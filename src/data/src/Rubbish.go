9cecc776301abe24c4edc67e804555ac6006c788
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-05-19 22:38:37 +0200

    Adding rubbish

--- src/data/src/Rubbish.go
+++ src/data/src/Rubbish.go
@@ -1,1 +1,24 @@
 package data
+
+import (
+	"errors"
+	"math"
+)
+
+//Average ... Calculate average of an array of integers.\n arr[] int is the sequence of numbers.\n negative is true, when you want to count negative numbers like negative numbers, and false otherwise.
+func Average(arr []int, negative bool) (float64, error) {
+	if len(arr) == 0 {
+		return 0.0, errors.New("Error parsing array of integers")
+	}
+	sum := 0.0
+	if negative {
+		for _, value := range arr {
+			sum += float64(value)
+		}
+	} else {
+		for _, value := range arr {
+			sum += math.Abs(float64(value))
+		}
+	}
+	return sum / float64(len(arr)), nil
+}
