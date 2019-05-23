a35a36dfaab42d86c2f5dc7fbd2cb0c8c9c04fcf
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-05-23 18:29:11 +0200

    Fixing login in database

--- src/data/src/script/initDatabase.sh
+++ src/data/src/script/initDatabase.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-mongoimport --username ${MONGO_INITDB_ROOT_USERNAME} --password ${MONGO_INITDB_ROOT_PASSWORD} --db ${MONGO_INITDB_DATABASE} --collection spain --type=csv --headerline --file /leagues/Spain/SP1.csv
+#mongo admin --eval "db.createUser({user: '$MONGO_INITDB_ROOT_USERNAME', pwd: '$MONGO_INITDB_ROOT_PASSWORD', roles:[{role:'readWrite',db:'$MONGO_INITDB_DATABASE'}]});"
+mongoimport --username $MONGO_INITDB_ROOT_USERNAME \
+            --password $MONGO_INITDB_ROOT_PASSWORD \
+            --authenticationDatabase $MONGO_INITDB_DATABASE \
+            --db leagues \
+            --collection spain \
+            --type=csv \
+            --headerline \
+            --file /leagues/Spain/SP1.csv
