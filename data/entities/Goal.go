cf13640e3c2bc3a6fbabfaaa970705da536570e1
estenoesmiputonombre <estonoesmiputocorreo@gmail.com>
2019-06-11 16:39:17 +0200

    Adding features

--- data/entities/Goal.go
+++ data/entities/Goal.go
@@ -24,7 +24,7 @@ type GoalError struct {
 
 var (
 	//DefaultLenGoal ... Default length of the arrays of struct goals
-	DefaultLenGoal int
+	DefaultLenGoal = 10
 	//DefaultPreviousGoals ... Default number of matchs before actual one
 	DefaultPreviousGoals = 1
 )
